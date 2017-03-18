058f291bb21fcd3c277b4dbc28bf9d95eb20a905
Alex Autem <autem.alex@gmail.com>
2017-03-17 22:14:13 -0400

    Update task controller to get, add and delete tasks

--- server/tasks/taskController.js
+++ server/tasks/taskController.js
@@ -2,20 +2,38 @@ var Task = require('./taskModel.js');
 
 module.exports = {
 
+  // get tasks for logged in user
   getTasks: function(req, res) {
-    // get tasks for logged in user
     var user = req.params.id;
+    Task.find({user: user}, function(err, tasks) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(tasks);
+      }
+    });
   },
 
+  // add task for logged in user
   addTask: function(req, res) {
-    // add task for logged in user
     var task = req.body.task;
     console.log('Task:', task);
+    task = new Task(task);
+    task.save(function(err, bracket) {
+      res.send(task);
+    });
   },
 
   removeTask: function(req, res) {
     // remove task for logged in user
     var id = req.params.id;
+    Task.remove({_id: id}, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send('Task Deleted.');
+      }
+    });
   }
 
 };
