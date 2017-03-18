7b15a1ae519d6b894af4fffabc61452ebc93b6e5
Alex Autem <autem.alex@gmail.com>
2017-03-18 01:19:04 -0400

    Add comments and console logs to task controller

--- server/tasks/taskController.js
+++ server/tasks/taskController.js
@@ -4,11 +4,13 @@ module.exports = {
 
   // get tasks for logged in user
   getTasks: function(req, res) {
+    console.log('*** GETTING TASKS ***')
     var user = req.params.id;
     Task.find({user: user}, function(err, tasks) {
       if (err) {
         console.log(err);
       } else {
+        console.log('*** TASKS FOUND ***', tasks);
         res.send(tasks);
       }
     });
@@ -16,21 +18,25 @@ module.exports = {
 
   // add task for logged in user
   addTask: function(req, res) {
+    console.log('*** ADDING TASK ***');
     var task = req.body;
     console.log('Task:', task);
     task = new Task(task);
-    task.save(function(err, bracket) {
+    task.save(function(err, task) {
+      console.log('*** TASK ADDED ***', task);
       res.send(task);
     });
   },
 
   // remove task for logged in user
   removeTask: function(req, res) {
+    console.log('*** REMOVING TASK ***');
     var id = req.params.id;
     Task.remove({_id: id}, function(err) {
       if (err) {
         console.log(err);
       } else {
+        console.log('*** TASK REMOVED ***');
         res.send('Task Deleted.');
       }
     });
